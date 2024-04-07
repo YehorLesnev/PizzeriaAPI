d9dd446e64a87db3e6c14ad3643eeb3d9743ed9f
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-04-07 12:33:01 +0300

    Add IncreasePhoneNumberMaxLength migration

---
+++ Pizzeria.Domain/Migrations/20240407093233_IncreasePhoneNumberMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pizzeria.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasePhoneNumberMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "staff",
+                type: "varchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "staff",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
