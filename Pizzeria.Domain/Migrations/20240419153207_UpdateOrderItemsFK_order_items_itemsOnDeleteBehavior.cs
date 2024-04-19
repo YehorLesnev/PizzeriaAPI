a0461a700889edf03e784fa1c5c3f88214b65eaf
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-04-19 18:36:24 +0300

    Fixed Item Delete

---
+++ Pizzeria.Domain/Migrations/20240419153207_UpdateOrderItemsFK_order_items_itemsOnDeleteBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pizzeria.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderItemsFK_order_items_itemsOnDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_order_items_items",
+                table: "order_items");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_order_items_items",
+                table: "order_items",
+                column: "item_id",
+                principalTable: "items",
+                principalColumn: "item_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_order_items_items",
+                table: "order_items");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_order_items_items",
+                table: "order_items",
+                column: "item_id",
+                principalTable: "items",
+                principalColumn: "item_id");
+        }
+    }
+}
